609a6dde5384cad371c9d8713bd428626d88af05
Randy Le <randohernandez0@gmail.com>
2020-07-08 17:30:42 -0700

    asdf

---
+++ QueryParserConsole/Query/QueryPlan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryParserConsole.Query
+{
+    public class QueryPlan
+    {
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        public List<IPlanStep> Steps { get; set; }
+        #endregion
+
+        #region Constructors
+        public QueryPlan()
+        {
+            Steps = new List<IPlanStep>();
+        }
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
---
+++ QueryParserConsole/Query/QueryPlanGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueryParserConsole.Query
+{
+    public class QueryPlanGenerator
+    {
+        #region Private Fields
+        #endregion
+
+        #region Public Properties
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        public QueryPlan GeneratePlan(IStatement statement)
+        {
+            var plan = new QueryPlan();
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+    }
+}
