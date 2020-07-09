31ae1b6162b2fe535b223d5c9fc952a758e18dbe
Randy Le <randohernandez0@gmail.com>
2020-07-09 07:42:03 -0700

    adsf

--- QueryParserConsole/Query/SelectQueryPlanGenerator.cs
+++ QueryParserConsole/Query/SelectQueryPlanGenerator.cs
@@ -7,6 +7,7 @@ using System.Threading;
 public class SelectQueryPlanGenerator
 {
     #region Private Fields
+    SelectStatement _statement;
     #endregion
 
     #region Public Properties
@@ -24,10 +25,39 @@ public class SelectQueryPlanGenerator
     #region Public Methods
     public QueryPlan GeneratePlan(SelectStatement statement)
     {
+        var plan = new QueryPlan();
+        _statement = statement;
+
+        var endStatements = GetEndStatements();
+
         throw new NotImplementedException();
     }
     #endregion
 
     #region Private Methods
+    private List<StatementPart> GetEndStatements()
+    {
+        var result = new List<StatementPart>();
+
+        foreach(var statement in _statement.Statements)
+        {
+            if (!statement.Text.Contains("("))
+            {
+                result.Add(statement);
+            }
+        }
+
+        return result;
+    }
+
+    private List<BoolStep> GetBooleanSteps(List<StatementPart> endStatements)
+    {
+        var result = new List<BoolStep>();
+
+        throw new NotImplementedException();
+
+        return result;
+    }
+
     #endregion
 }
